fff2b742a81ec6cf8e1f0fb64eaff5955dff8092
Ian <ijr011235@gmail.com>
2025-11-25 14:07:12 -0700

    make code modular

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
 use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config: Config = Config::build(&args).unwrap_or_else(|err: &'static str| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file path {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
 }
 
